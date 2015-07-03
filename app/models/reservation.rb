d90abb52a921c794c938d6f53ba732dc0e415133
Juan Pablo <jblanco@despegar.com>
2015-07-03 16:33:39 -0300

    todo list

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,7 +2,8 @@ class Reservation < MiDespegarRestModel
   def self.find(id)
     response = self.http_get("http://localhost:3001/fenix-cp-rc/api/transactions/#{id}/summary")
 
-    reservation = {id: id, mail: country: products}
+    #TODO: amasar el response y cargar la variable reservation solo con lo que se necesita del lado de angular
+    reservation = {id: id}
     reservation
   end
 end
